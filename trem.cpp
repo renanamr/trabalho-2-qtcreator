0d7fae6d8052602333a590a43db9e493da280f9a
Artur Santiago <artursantiago2001@hotmail.com>
2021-03-18 22:39:47 -0300

    initial commit

---
+++ trem.cpp
@@ -0,0 +1,84 @@
+#include "trem.h"
+#include <QtCore>
+
+//Construtor
+Trem::Trem(int ID, int x, int y){
+    this->ID = ID;
+    this->x = x;
+    this->y = y;
+    velocidade = 100;
+}
+
+//Função a ser executada após executar trem->START
+void Trem::run(){
+    while(true){
+        switch(ID){
+        case 1:     //Trem 1
+            if (y == 30 && x <330)
+                x+=10;
+            else if (x == 330 && y < 150)
+                y+=10;
+            else if (x > 130 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 2: //Trem 2
+            if (y == 30 && x <530)
+                x+=10;
+            else if (x == 530 && y < 150)
+                y+=10;
+            else if (x > 330 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 3: //Trem 3
+            if (y == 30 && x <730)
+                x+=10;
+            else if (x == 730 && y < 150)
+                y+=10;
+            else if (x > 530 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 4: //Trem 4
+            if (y == 150 && x <430)
+                x+=10;
+            else if (x == 430 && y < 270)
+                y+=10;
+            else if (x > 230 && y == 270)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 5: //Trem 5
+            if (y == 150 && x <630)
+                x+=10;
+            else if (x == 630 && y < 270)
+                y+=10;
+            else if (x > 430 && y == 270)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        default:
+            break;
+        }
+        msleep(velocidade);
+    }
+}
+
+void Trem::mudarVelocidade(int velocidade) {
+    msleep(velocidade);
+}
+
+
+
+
